de1a25dfdcb1189e1400fa97734d2a15a62ff4da
Abhay 076 <yadavabhaycs@gmail.com>
2022-05-30 08:57:08 +0530

    modules

---
+++ Back-End/nodews/modules_node_js/index.js
@@ -0,0 +1,3 @@
+const operation=require('./operations');//function is used to include modules and libraries in Node Js
+console.log(operation.add(2,3));
+console.log(operation.multiply(2,3));
---
+++ Back-End/nodews/modules_node_js/operations.js
@@ -0,0 +1,7 @@
+module.exports.add=function(a,b){
+    return a+b;
+}
+// Simple or complex functionality organized in a single or multiple JavaScript files which can be reused throughout your Node.js application is called module
+module.exports.multiply=function(a,b){
+    return a*b;
+}
